6b239bba510ca149a8ba96f773432d67f7089d34
Luam Belay <luam.belay@gmail.com>
2023-12-08 22:28:09 +0100

    Couldn't do the demo

---
+++ javascript/js-basics/demo-start/js/index.js
@@ -0,0 +1,12 @@
+// Selecting HTML Elements: .querySelector()
+
+console.clear();
+require("/Users/luambelay/Desktop/spiced-bootcamp/session-notebook/javascript/js-basics/demo-start/js/index.js");
+
+// Preferred method
+const mainElement = document.querySelector('[data-js="main"]');
+
+// Other methods
+// const mainElement = document.querySelector("main");
+// const mainElement = document.querySelector(".main");
+// const mainElement = document.querySelector("#main");
